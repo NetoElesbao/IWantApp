3e0171126532495033f83a263889747015f11433
est_jelesbao <josenetu32@gmail.com>
2023-09-18 17:59:56 -0300

    Version 1.2 - last study of Dapper and Entity, methods of query

--- Endpoints/Employees/EmployeesGetAll.cs
+++ Endpoints/Employees/EmployeesGetAll.cs
@@ -15,38 +15,40 @@ namespace IWantApp.Endpoints.Employees
         public static string[] HttpMethods => new string[] { HttpMethod.Get.ToString() };
         public static Delegate Handler => Action;
 
+        //ENDPOINT DE SOLICITAÇÃO DE USUARIOS USANDO O ENTITY, COM PAGINAÇÃO
+        // public static IResult Action(int page, int rows, UserManager<IdentityUser> manager)
+        // {
+        //     var users = manager.Users.Skip((page - 1) * rows).Take(rows).ToList();
+        //     var usersDTO = new List<EmployeeResponseDTO>();
 
-        public static IResult Action(int page, int rows, UserManager<IdentityUser> manager)
-        {
-            var users = manager.Users.Skip((page - 1) * rows).Take(rows).ToList();
-            var usersDTO = new List<EmployeeResponseDTO>();
+        //     foreach (var user in users)
+        //     {
+        //         var claims = manager.GetClaimsAsync(user).Result;
+        //         var claim = claims.FirstOrDefault(e => e.Type.Equals("Name"));
+        //         var claimValidation = claim != null ? claim.Value : string.Empty;
+        //         if (claimValidation.Any()) { usersDTO.Add(new EmployeeResponseDTO(user.Email, claimValidation)); }
+        //     }
 
-            foreach (var user in users)
-            {
-                var claims = manager.GetClaimsAsync(user).Result;
-                var claim = claims.FirstOrDefault(e => e.Type.Equals("Name"));
-                var result = claim != null ? claim.Value : string.Empty;
-                usersDTO.Add(new EmployeeResponseDTO(user.Email, result));
-            }
-            return Results.Ok(usersDTO);
-        }
+        //     return Results.Ok(usersDTO.OrderBy(e => e.Name));
+        // }
 
+        //ENDPOINT DE SOLICITAÇÃO DE USUARIOS USANDO O DAPPER, COM PAGINAÇÃO
         public static IResult Action(int page, int rows, IConfiguration configuration)
         {
-            var database = new SqlConnection(configuration.GetConnectionString("Connection"));
-            var users = database.Query<EmployeeResponseDTO>
+            var databse = new SqlConnection(configuration.GetConnectionString("Connection"));
+
+            var query = databse.Query
             (
-                @"select Email, ClaimValue as Name 
-                  from AspNetUsers a inner join AspNetUserClaims b
-                  on a.Id = b.UserId and ClaimType = 'Name'
-                  order by Name
-                  OFFSET (@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY",
-                  new { page, rows }
-
+                @"select Email as 'Emails', ClaimValue as 'Names'
+                from AspNetUsers a inner join AspNetUserClaims b
+                on a.Id = b.UserId and ClaimType = 'Name' 
+                order by Names
+                offset (@page - 1) * @rows rows fetch next @rows rows only ",
+                new {page, rows}
             );
-
-            return Results.Ok(users);
+            return Results.Ok(query);
         }
+
     }
 }
 
